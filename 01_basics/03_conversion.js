a2230a872c5d073d8d83d80e5e17761f415dbd5a
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-08-13 11:33:02 +0000

    commit

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -12,3 +12,21 @@ let isScore = 12;
 
 let isNumber = Boolean(isScore);
 console.log(isNumber);
+
+let str = "";
+let isBoolean = Boolean(str)
+console.log(isBoolean);
+
+//empty string is false.
+
+//***************************OPERATION************//
+
+console.log('1' + 2);
+console.log(2 + "6");
+console.log('8' + "6");
+console.log(1 + 2 + '5');
+console.log(1 + 2 + '5' + 1 + 9);
+//all these are typecoersion in which JS treat everything a string after a stringValue.
+
+console.log("" + 1);//" " is treated as 0;
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);// JS treats null as 0 ;
+
+//undefined is always 0; no matter wt.
+console.log('h');
+console.log('1' === 1); //=== strict ekual  dont do typeCoersion.
+
+console.log('1' == 1);//typeCoersion is done by JS

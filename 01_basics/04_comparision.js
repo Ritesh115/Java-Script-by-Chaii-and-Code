cd2c74fbb3dd085a4e890b0b57f717b40b7a28e9
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-08-13 12:01:12 +0000

    commit

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -7,3 +7,22 @@ console.log('h');
 console.log('1' === 1); //=== strict ekual  dont do typeCoersion.
 
 console.log('1' == 1);//typeCoersion is done by JS
+
+
+//***************************** *//
+
+//Reference type datatypes
+//1. ARRAY 2. OBJECT 3. FUNCTIONS  ALL have type object..
+
+//BIGINT
+
+let isval = 541154644454685468n;//by postfixing n at last becames bigint
+console.log(typeof isval);
+
+
+/***************************************** */
+
+// STACK   ,  HEAP are two types of memory
+//stack is used in premitive
+//heap in reference type
+// if we give access to any other variable it will get the address of the heap same address..

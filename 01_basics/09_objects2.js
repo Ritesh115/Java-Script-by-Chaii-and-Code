62f4c49e603d5a297fb2598f9802ff5801964e0f
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-09-04 13:01:52 +0000

    commit

---
+++ 01_basics/09_objects2.js
@@ -0,0 +1,39 @@
+console.log('objects 2');
+ 
+const obj = new Object();
+obj.name = 'RItesh';
+obj.age = 24;
+obj.gender = 'Male';
+obj.greeting = ()=>{
+  console.log('hello there ...');
+}
+
+console.log(obj);
+
+// how to merge to objects
+
+const obj1 = {1:'a',2:'b'};
+const obj2 = {3:'c',4:'d'};
+
+// const obj3 = {obj1,obj2};
+// console.log(obj3)
+
+//we use assign function
+
+// const obj3 = Object.assign( {}, obj1 , obj2); // it assign the results in obj1 to handle this we use {} empty object all values assign in it.
+// console.log(obj3);
+
+
+
+const obj3 = {...obj1 , ...obj2}
+console.log(obj3);
+
+
+
+//WE CAN FIND KEYS AND VALUES Also  imp
+
+console.log(Object.keys(obj1));
+console.log(Object.values(obj2));
+
+
+

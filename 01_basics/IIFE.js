6eee5db420f0ffc8b528511255c34f9c8ca3599f
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-09-06 09:39:22 +0000

    iife

--- 01_basics/IIFE.js
+++ 01_basics/IIFE.js
@@ -1,1 +1,37 @@
-console.log('IIFE');
+
+//if function is returning a line answer so we can do this;
+const addTwo = (num1,num2) => ([1,2,3,4,5])
+//USING () bracket we can return anything an [], object or some value
+console.log(addTwo(2,3));
+
+
+console.log("iife");
+//immediatly invoked function expression
+
+
+(function chaii(){ // named iife
+   console.log("DB CONNECTED");
+}) (); // we wrap function in () bracket and then call it immediatly  by ().
+
+//GLOBAL VAR ke pollution se bachna ke liya we use iife.
+
+
+((name) => { //unnamed iife
+  console.log("DB2 CONNECTED",name);
+}) ('ritesh')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

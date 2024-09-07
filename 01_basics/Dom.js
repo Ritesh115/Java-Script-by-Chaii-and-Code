c085368328184d4e1f78341bc55edfe6c57a9ad2
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-09-07 10:15:32 +0000

    co

---
+++ 01_basics/Dom.js
@@ -0,0 +1,5 @@
+//DOM 
+// document object model => when we want to manipulate HTML file  with the help of  js we use DOM;
+//jo bhi document ham bana rahe hai like html , vo sab ak object he to hai like head and body ..and vo document kis tara se structure hai is DOM 
+
+ 

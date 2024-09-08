323527a2f82591172315411fade991ba82903b4a
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-09-08 09:49:31 +0000

    commit

---
+++ 01_basics/children_dom.js
@@ -0,0 +1,8 @@
+//docyment.children => gives all the access of chileelement inside parentelement..
+//console.log(element.children[1].innerHTML);
+
+//IMP
+
+//Important discussion about NodeList. 
+//A nodelist is a collection of document nodes which includes text nodes , attribute nodes and element nodes. A line break is also counted in nodelist but only first one. All subsequent line breaks are ignored by browser..
+//thats why JS is complex becs it forms a complex tree ..

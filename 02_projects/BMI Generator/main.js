81750f04758a553f43c32ea0eb9418066c21b5ec
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-09-09 12:20:39 +0000

    commit

---
+++ 02_projects/BMI Generator/main.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+//Form is sybmitted in post and get type go to server or url..
+//we have to access it beffore going to server
+
+form.addEventListener( 'submit' , (e) => {
+    e.preventDefault()
+    //we need values
+
+    
+
+} );

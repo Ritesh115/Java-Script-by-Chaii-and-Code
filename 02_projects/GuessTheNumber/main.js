afe3e033d009d235859dd07ca4e274898588c303
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-09-11 11:16:32 +0000

    commit

--- 02_projects/GuessTheNumber/main.js
+++ 02_projects/GuessTheNumber/main.js
@@ -1,10 +1,33 @@
-const min = 1;
-const max = 100;
-const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
-
+//we need no ypto 100 thats why *100
+const randomNum = parseInt(Math.random() * 100 + 1);
+console.log(randomNum);
 const form = document.querySelector('.form')
+let i = 10;
+form.addEventListener( 'submit' , (e) =>{
+  e.preventDefault();
 
-form.addEventListener( submit , (e) =>{
-  console.log(e);
-  console.log(e.target);
-}  );
+  const guessField = parseInt(document.querySelector('.guessField').value);
+
+  if(guessField === randomNum){
+      document.querySelector('.lowOrHi').innerHTML = 'right g';
+  }
+  else {
+    document.querySelector('.lowOrHi').innerHTML = 'wrong g'
+    
+  while(i >= 1){
+    i--;
+    document.querySelector('.lastResult').innerHTML = i;
+    if(i !== 0) {
+      break;
+    }
+    else{
+      document.querySelector('.lastResult').innerHTML = 'no more guess left';
+      document.querySelector('.lastResult').setAttribute('disabled="disabled"')
+     disabled="disabled"
+    }
+  }
+
+  }
+});
+
+

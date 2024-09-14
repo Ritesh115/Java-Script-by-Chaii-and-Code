11ef56bc94adb4790e940daafeafd751e6b8efbf
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2024-09-14 06:52:26 +0000

    commit

--- 02_projects/GuessTheNumber/main.js
+++ 02_projects/GuessTheNumber/main.js
@@ -1,33 +1,84 @@
 //we need no ypto 100 thats why *100
-const randomNum = parseInt(Math.random() * 100 + 1);
-console.log(randomNum);
-const form = document.querySelector('.form')
-let i = 10;
-form.addEventListener( 'submit' , (e) =>{
-  e.preventDefault();
+let randomNumber = parseInt(Math.random() * 100 + 1);
 
-  const guessField = parseInt(document.querySelector('.guessField').value);
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
 
-  if(guessField === randomNum){
-      document.querySelector('.lowOrHi').innerHTML = 'right g';
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+//first to check whether yser is ready to play game or not.
+if (playGame) {
+  submit.addEventListener('click', function (e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
+  })
+}
+
+function validateGuess(guess) {
+  if(isNaN(guess)){
+alert('please eneter a valid number')
   }
-  else {
-    document.querySelector('.lowOrHi').innerHTML = 'wrong g'
-    
-  while(i >= 1){
-    i--;
-    document.querySelector('.lastResult').innerHTML = i;
-    if(i !== 0) {
-      break;
-    }
-    else{
-      document.querySelector('.lastResult').innerHTML = 'no more guess left';
-      document.querySelector('.lastResult').setAttribute('disabled="disabled"')
-     disabled="disabled"
-    }
+  else if(guess<1){
+ alert('please enter a number more than 1')
   }
+  else if(guess>100){
+    alert('please enter a number less than 100')
+     }
+     else{
+      prevGuess.push(guess);
 
+      if(numGuess === 11){//if it is last gyess of yser.
+    displayGuess(guess);
+    displayMessage(`Game is over . Random number was ${randomNumber}`)
+    endGame();
+        }
+        else{
+          displayGuess(guess);
+          checkGuess(guess);
+        }
+     }
+}
+
+
+function checkGuess(guess) {
+  //to check valye is ekyal or low or high;
+  if(guess === randomNumber){
+    displayMessage(`Yoy gyessed it rigth`)
+    endGame();
   }
-});
+  else if(guess < randomNumber){
+    displayMessage(`Number is TOO low`);
+  }
+  else(guess > randomNumber){
+    displayMessage(`Number is TOO High`);
+  }
+}
 
 
+function displayGuess(guess) {
+  //the things we are going to display or its cleanyp .
+  userInput.value = '';
+}
+
+function displayMessage(message) {
+  
+}
+
+function endGame() {
+  
+}
+
+function newGame() {
+  
+}
